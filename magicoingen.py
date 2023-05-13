f9088349c3f16f58784d0cfb198007d371087b98
Zoyberg S. Mith <gmanpowershell@gmail.com>
2023-05-13 10:31:43 +0300

    Добавил программу

---
+++ magicoingen.py
@@ -0,0 +1,16 @@
+coins = int(input("Введите количество монет: ")) # Количество монет
+year = coins / 2 * 365
+year = int(year) # изменяем float в interger
+total = year + coins # делаем финальное вычисление
+print("Вы заработаете:", total, "монет в течении 1 года")
+crow = int(input("Но Ворона, падла, будет у вас тырить: "))
+crowweek = crow * 52
+total = total - crowweek
+if crowweek > 0 :
+    print("Но ебучая ворона спиздит все твои деньги и ты будешь должен:", total, "рублей.")
+else crowweek < 0:
+    print("В общем, ты получишь", total, "рублей")
+else crowweek == 0:
+    print("Хуясе ты снайпер!")
+fi
+
